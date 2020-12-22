e56b6fd72ab5ac15adb8e23eb88e2ab0bc521909
CraigStueber <craigstueber@gmail.com>
2020-12-22 06:11:46 -0500

    added 1numbers changed color scheme

--- 7days.php
+++ 7days.php
@@ -9,7 +9,7 @@ $message = $_POST['message'];
 $email_subject = 'New form submission';
 $email_body = "User First Name : $name \n" .
     "User Last Name : $lname \n" .
-    "User Email : $email \n" .
+    "User Email : $emailFrom \n" .
     "User Phone Number : $phone \n" .
     "Property Address : $address \n" .
     "Message: $message \n";
